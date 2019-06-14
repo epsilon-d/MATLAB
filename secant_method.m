3bfac692cf5905a2d937937fea80c0a525cab1aa
epsilon-d <49400045+epsilon-d@users.noreply.github.com>
2019-06-15 03:12:13 +0900

    Update secant_method.m

--- secant_method.m
+++ secant_method.m
@@ -1,4 +1,4 @@
-% Copyright(c)2019 epsilon-d All rights reserved.
+% Copyrightⓒ2019 epsilon-d All rights reserved.
 % This program is program to find solution of f(x) = x^3 + 2*x^2 - 1 using secant method.
 % Suppose that we know that f(0) = -1 < 0 and f(1) = 2 > 0
 
@@ -7,23 +7,23 @@ clc
 close all
 
 x = 1;
-y = fnc_se(x);
+y = fnc_secant(x);
 
 % initial value = 0, 1
 p1 = 0;
 p2 = 1;
 p3 = 1;
 
-while abs(fnc_se(p3)) > 0.0001
+while abs(fnc_secant(p3)) > 1e-6
     
-    p3 = p1 - ( fnc_se(p1)*(p2 - p1)/( fnc_se(p2) - fnc_se(p1) ) );
+    p3 = p1 - ( fnc_secant(p1)*(p2 - p1)/( fnc_secant(p2) - fnc_secant(p1) ) );
 
     comment_1 = 'p3 is %f\n';
     comment_2 = 'f(p3) is %f\n\n';
     fprintf(comment_1,p3)
     fprintf(comment_2,p3)
     
-    if fnc_se(p3) > 0
+    if fnc_secant(p3) > 0
         p2 = p3;
     else
         p1 = p3;
@@ -34,6 +34,6 @@ end
 comment_final = 'Final solution is %f\n';
 fprintf(comment_final,p3)
 
-function y = fnc_se(x)
+function y = fnc_secant(x)
 y = x^3+2*x^2-1;
 end
